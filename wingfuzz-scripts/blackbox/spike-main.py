03cf77a0242e61b8f89796ae17ea7398412a8f55
orlando <dez23@mails.tsinghua.edu.cn>
2024-04-19 04:44:39 -0700

    [FEATURE]: init version of spike-main.

--- wingfuzz-scripts/blackbox/spike-main.py
+++ wingfuzz-scripts/blackbox/spike-main.py
@@ -1,4 +1,5 @@
 import os
+import gc
 import sys
 import getopt
 import socket
@@ -16,12 +17,14 @@ PROTOCOL = "ftp"
 WORK_DIR = "~/wingfuzz"
 BINARY = "proftpd_v1.3.8"
 SUM_BITMAP = b''
+IN_DIR = f"../../{PROTOCOL}/in/"
 # ===== Spike Params =====
 #exclude = ['TRUN','STATS','TIME','SRUN','HELP','EXIT','GDOG']
 EXCLUDE = []
 SKIPSTR = 0
 SKIPVAR = 0
 SPKS_DIR = '~/wingfuzz/ftp/conf'
+DURATION_TIME = 3600
 # Running spike scripts using the TCP/UDP script interpreter 
 # spike-fuzzer-generic-send_tcp / spike-fuzzer-generic-send_udp
 BIN = '~/Spike-Fuzzer/usr/bin/spike-fuzzer-generic-send_tcp'
@@ -110,7 +113,8 @@ def run_spike():
                 os.system(f'{BIN} {PROXY_IP} {PROXY_PORT} {newfile} {SKIPSTR} {SKIPVAR} >log 2>&1')
 
 
-def fuzz_application(server):
+def fuzz_application_duration(server, duration):
+    start = time.time()
     #create client socket connection
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((TARGET_IP, TARGET_PORT))
@@ -119,7 +123,7 @@ def fuzz_application(server):
     client_handler = threading.Thread(target=run_spike, args=())
     client_handler.start()
 
-    while True:
+    while time.time() - start < duration:
         #Accept the connection from localhost to proxy and send the socket to handle_client_connections
         client_sock, _ = server.accept()
         handle_client_connection(client_sock)
@@ -135,6 +139,7 @@ def fuzz_application(server):
             record_path = './record.txt'
             SUM_BITMAP = update_sum_bitmap(bitmap, SUM_BITMAP, record_path)
 
+
 def usage():
     print("Spike Fuzzing Proxy \nUsage: spike-proxy.py -l proxy_ip:port -t target_ip:port -d spikefiles_directory -e excludes \n\
     -l --local               -Set up local proxy on this ip:port \n\
@@ -147,7 +152,7 @@ def usage():
     sys.exit(0)
 
 
-if __name__ == "__main__":
+def spike_cmd_boot():
     if not len (sys.argv[1:]):
         usage()
 
@@ -190,8 +195,23 @@ if __name__ == "__main__":
             else:
                 EXCLUDE.append(a)
 
-    print("")
-    print("[INFO] Starting Spike Proxy")
+    print("\n[INFO] Starting Spike Proxy")
+
+
+# Record message from grey-box
+def record_msg(b_msg):
+    now = datetime.now()
+    formatted_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+    print(f"Black_Box_Fuzzing Get MSG - {formatted_time}")
+    print(f"MSG - {b_msg}")
+    file = os.path.join(IN_DIR, f"Grey-Box-{formatted_time}.raw")
+    with open(file, 'wb') as f:
+        f.write(b_msg)
+        time.sleep(2)  # need some time to write
+
+
+if __name__ == "__main__":
+    spike_cmd_boot()
     
     # Create SHM to record Coverage
     shmid = open_shm()
@@ -207,7 +227,41 @@ if __name__ == "__main__":
         print(f'[INFO] __AFL_SHM_ID={str(shmid)}')
         print(f'[INFO] Spike Proxy Listening on {PROXY_IP}:{PROXY_PORT}')
 
-        fuzz_application(server)
+        for idx in range(0, 10):
+            # Prevent OOM
+            gc.collect()
+
+            # TODO: need initial specific seeds
+            fuzz_application_duration(server, DURATION_TIME)
+
+            conn, addr = server.accept()
+            with conn:
+                flag = conn.recv(4)
+                while True:
+                    # Sticky Packets and Packet Splitting
+                    if flag.decode('utf-8') == 'mesg':
+                        flag = bytes()  # set as null
+                        test = conn.recv(8).decode('utf-8')
+                        msg_len = test.split('.')[0]
+                        part_msg = test.split('.')[1]
+                        res_data_len = int(msg_len) - 7 + len(msg_len)
+                        recv_data = b''
+                        while len(recv_data) < res_data_len:
+                            data = conn.recv(res_data_len - len(recv_data))
+                            if not data:
+                                break
+                            recv_data += data
+                        msg = part_msg + recv_data.decode('utf-8')
+                        b_msg = bytes(msg, 'latin-1').decode('unicode_escape').encode('latin-1')
+                        record_msg(b_msg)
+                        # read another flag
+                        flag = conn.recv(4)
+                    elif flag.decode('utf-8') == 'stop' or flag == bytes():
+                        now = datetime.now()
+                        formatted_time = now.strftime("%Y-%m-%d-%H-%M-%S")
+                        print(f"Black_Box_Fuzzing Quit - {formatted_time}")
+                        #stop_thread = True  
+                        break
     
     p = execute(program_close)
     close_shm(shmid)
