3b8ae414fefecceef951bed745c17a17a83cd3c4
orlando <dez23@mails.tsinghua.edu.cn>
2024-04-30 00:56:10 -0700

    [FIX]: fix func name.

--- wingfuzz-scripts/blackbox/peach-nowing.py
+++ wingfuzz-scripts/blackbox/peach-nowing.py
@@ -1,11 +1,11 @@
-import os
+
 import threading
 from utils import *
 from spiutils import *
 
 ''' ------------< PEACH AND TARGET CONFIGURATION >------------ '''
 # ===== Network Params =====
-TARGET_IP = '0.0.0.0' # local/remote machine
+TARGET_IP = '127.0.0.1' # local/remote machine
 TARGET_PORT = 5300
 # ===== Target Params =====
 PROTOCOL = "dns"
@@ -20,7 +20,7 @@ BIN = '~/peach-3.1.124/peach'
 
 files_run = []
 
-def run_spike():
+def run_peach():
     flist = os.listdir(PITS_DIR)
     for file in sorted(flist):
         name = file.split('.')
@@ -28,7 +28,19 @@ def run_spike():
             newfile = PITS_DIR + '/' + file
             print(f"[INFO] Fuzzing {TARGET_IP}:{TARGET_PORT} Using {file}")
             files_run.append(name[0])
-            os.system(f'{BIN} -c {newfile} >peach_log 2>&1')
+            os.system(f'{BIN} {newfile} >peach_log 2>&1')
+
+
+def cov_count():
+    global SUM_BITMAP
+    bitmap = get_bitmap(shmid)
+    clean_shm(shmid)
+
+    if SUM_BITMAP == b'':
+        SUM_BITMAP = bitmap
+    else:
+        record_path = './record.txt'
+        SUM_BITMAP = update_sum_bitmap(bitmap, SUM_BITMAP, record_path)
 
 
 # Peach-nowing, just blackbox fuzzing
@@ -44,7 +56,7 @@ if __name__ == "__main__":
 
     print(f'[INFO] __AFL_SHM_ID={str(shmid)}')
 
-    client_handler = threading.Thread(target=run_spike(), args=())
+    client_handler = threading.Thread(target=run_peach(), args=())
     client_handler.start()
 
     try:
